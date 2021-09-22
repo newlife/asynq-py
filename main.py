3dd1a32e82333b0270912ce107863845879a7f0a
Newlife <shfeng819@126.com>
2021-09-22 19:39:44 +0800

    add new dependency schedule

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ from client import Client
 from task import Task, Option
 import time
 
-task = Task("email:welcome", {"UserID": 222})
+# task = Task("email:welcome", {"UserID": 222})
+# c = Client()
+# c.enqueue(task)
+
+
+task = Task("job:expired",{})
 c = Client()
 c.enqueue(task)
 
-option = Option()
-at = time.time() + 10
-option.process_at = at
-task = Task("email:welcome", {"UserID": 232})
-c.enqueue(task, option=option)
+# option = Option()
+# at = time.time() + 10
+# option.process_at = at
+# task = Task("email:welcome", {"UserID": 232})
+# c.enqueue(task, option=option)
 
-option.process_at += 10
-task = Task("email:welcome", {"UserID": 242})
-c.enqueue(task, option=option)
+# option.process_at += 10
+# task = Task("email:welcome", {"UserID": 242})
+# c.enqueue(task, option=option)
